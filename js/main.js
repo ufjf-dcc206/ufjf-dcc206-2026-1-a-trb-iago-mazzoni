4afbd178621e8214096edb020fa13c9e70fa5ba8
IagoMazzoni <iagomazzoni@gmail.com>
2026-07-02 22:29:36 -0300

    Criei agora a logica do jogo, ja calcula a quantidade de vizinhas com bomba e gera o tabuleir bunitin com as bombas bem randon

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 import './components/campoMinadoCelula.js';
-import { DIFICULDADES, criarTabuleiro, distribuirMinas } from './motor/logicaJogo.js';
+import { DIFICULDADES, criarTabuleiro, distribuirMinas, calcularVizinhas } from './motor/logicaJogo.js';
 
-// Teste 
 const { linhas, colunas, minas } = DIFICULDADES.facil;
 const tabuleiro = criarTabuleiro(linhas, colunas);
 const comMinas = distribuirMinas(tabuleiro, minas);
+const comVizinhas = calcularVizinhas(comMinas);
 
-console.log('Tabuleiro gerado:', comMinas);
-console.log('Total de minas:', comMinas.flat().filter(c => c.temMina).length);
+console.log('Tabuleiro final:', comVizinhas);
+
+// Mostra uma versão simplificada no console pra visualizar melhor:
+// '💣' onde tem mina, o número de vizinhas onde não tem
+const visual = comVizinhas.map(linha =>
+  linha.map(c => c.temMina ? '💣' : c.vizinhas || ' ').join(' ')
+).join('\n');
+
+console.log('Mapa:\n' + visual);
--- js/motor/logicaJogo.js
+++ js/motor/logicaJogo.js
@@ -47,3 +47,49 @@ export const DIFICULDADES = {
   
     return novo;
   }
+
+
+export function calcularVizinhas(tabuleiro) {
+  const linhas = tabuleiro.length;
+  const colunas = tabuleiro[0].length;
+
+
+  const novo = tabuleiro.map(linha =>
+    linha.map(celula => ({ ...celula }))// tal do spread operator que copia(espalha) todas as propriedades do objeto celula dentro do novo objeto
+  );
+// não da para fazer const copia = [...tabuleiro]; porque as linhas dentro seriam as mesmas do original, então a alteração em um se propagaria para o outra tbm
+// nesse caso o map percorre cada linha e dentro de cada linha ele percorre cada celula fazendo um spread, ou seja copiando tudo. Isso garante que estou trabalhando em uma copia fiel e não a mesma linha na memoria
+
+
+  const direcoes = [
+    [-1, -1], [-1, 0], [-1, 1],
+    [ 0, -1],          [ 0, 1],
+    [ 1, -1], [ 1, 0], [ 1, 1],
+  ];
+
+  for (let l = 0; l < linhas; l++) {
+    for (let c = 0; c < colunas; c++) {
+
+      if (novo[l][c].temMina) continue;
+
+      let count = 0;
+
+      for (const [dl, dc] of direcoes) {//esse carinha aqui vai, para cada combinação de l c variada dentro do duplo for anterior, verificar cada posição do vetor direcoes
+      // então a para é que [dl,dc] recebe então [-1,-1] que é a primeira posição do vetor direcoes e para cada instancia do for ele vai para a seguinte  
+        const nl = l + dl; 
+        const nc = c + dc; 
+
+
+        const dentroDoLimite = nl >= 0 && nl < linhas && nc >= 0 && nc < colunas;
+
+        if (dentroDoLimite && novo[nl][nc].temMina) {
+          count++;
+        }
+      }
+
+      novo[l][c].vizinhas = count;
+    }
+  }
+
+  return novo;
+}
